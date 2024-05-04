a10b8a5dd146ad6ccb95c35c5e98b5ef9987ac58
csinfoti <cs.infoti@gmil.com>
2024-05-04 10:39:39 -0300

    aula 0405 part1

---
+++ src/app/lancamentos/despesas/despesas.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-despesas',
+  templateUrl: './despesas.component.html',
+  styleUrl: './despesas.component.scss'
+})
+export class DespesasComponent {
+
+}

f56e9707a70c51bcd490685379b46611a44fafac
csinfoti <cs.infoti@gmil.com>
2024-05-06 13:29:43 -0300

    0405

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ReceitasComponent } from './Relatorios/receitas/receitas.component';
+import { DespesasComponent } from './Relatorios/despesas/despesas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'login', pathMatch: 'full' }, //redireciona rota
+  {path: 'login', component: LoginComponent},
+  {path: 'cadastro', component: CadastroComponent},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'notfound', component: PageNotFoundComponent},
+  {path: 'relatorios/despesas', component: DespesasComponent },
+  {path: 'relatorios/receitas', component: ReceitasComponent },
+  {path: 'lancamentos', loadChildren: () => import('./lancamentos/lancamentos.module').then(m =>
+    m.LancamentosModule)}, 
+  { path: '**', component: PageNotFoundComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/lancamentos/lancamentos-routing.module.ts
+++ src/app/lancamentos/lancamentos-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ReceitasComponent } from './receitas/receitas.component';
+import { DespesasComponent } from './despesas/despesas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'receitas', component: ReceitasComponent},
+  {path: 'despesas', component: DespesasComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
